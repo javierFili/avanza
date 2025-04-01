44a671b2b8e859989afce9e5f61412f49a7d1725
= <=>
2025-03-31 20:20:46 -0400

    fix:migracion de led_pipeline_user

--- packages/Webkul/Lead/src/Database/Migrations/2025_03_25_120303_create__lead_pipeline_user.php
+++ packages/Webkul/Lead/src/Database/Migrations/2025_03_25_120303_create__lead_pipeline_user.php
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('_lead_pipeline_user');
+        Schema::dropIfExists('lead_pipeline_user');
     }
 };
