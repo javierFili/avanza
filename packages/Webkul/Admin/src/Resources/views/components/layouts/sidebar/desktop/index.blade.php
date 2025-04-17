7b7b9b1356fee68869e283bc8f32c762e32f0583
= <=>
2025-04-16 20:22:53 -0400

    repositorio

--- packages/Webkul/Admin/src/Resources/views/components/layouts/sidebar/desktop/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/layouts/sidebar/desktop/index.blade.php
@@ -7,6 +7,11 @@
     <div class="journal-scroll h-[calc(100vh-100px)] overflow-hidden group-[.sidebar-collapsed]/container:overflow-visible">
         <nav class="sidebar-rounded grid w-full gap-2">
             <!-- Navigation Menu -->
+            @php
+                // En tu vista temporalmente:
+                //dd(menu()); // Verás el objeto completo
+                dd(debug_backtrace()); // Verás de dónde se llama
+            @endphp
             @foreach (menu()->getItems('admin') as $menuItem)
                 <div class="px-4 group/item {{ $menuItem->isActive() ? 'active' : 'inactive' }}">
                     <a
@@ -20,7 +25,7 @@
 
                         <div class="flex-1 flex justify-between items-center text-gray-600 dark:text-gray-300 font-medium whitespace-nowrap group-[.sidebar-collapsed]/container:hidden {{ $menuItem->isActive() ? 'text-white' : ''}} group">
                             <p>{{ core()->getConfigData('general.settings.menu.'.$menuItem->getKey()) ?? $menuItem->getName() }}</p>
-                        
+
                             @if ( ! in_array($menuItem->getKey(), ['settings', 'configuration']) && $menuItem->haveChildren())
                                 <i class="icon-right-arrow rtl:icon-left-arrow invisible text-2xl group-hover/item:visible {{ $menuItem->isActive() ? 'text-white' : ''}}"></i>
                             @endif
