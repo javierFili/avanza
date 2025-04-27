90217b8fff95862cdb8779b18e31fc566a52af51
= <=>
2025-04-27 10:30:00 -0400

    muestara de user

--- packages/Webkul/Lead/src/Repositories/PipelineRepository.php
+++ packages/Webkul/Lead/src/Repositories/PipelineRepository.php
@@ -6,7 +6,6 @@ use Illuminate\Container\Container;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 use Webkul\Core\Eloquent\Repository;
-use Webkul\Lead\Models\Pipeline;
 use Webkul\User\Repositories\UserRepository;
 
 class PipelineRepository extends Repository
@@ -120,32 +119,35 @@ class PipelineRepository extends Repository
 
         return $pipeline;
     }
-
     /**
      *get all pipelines by user_id
-     */
-    public function getAllPipelinesByUserId($userId)
-    {
-        $user = $this->userRepository->where('id', $userId)->with('leadPipelines')->first();
-        $pipelines = $user?->leadPipelines;
-
+    */
+    public function getAllPipelinesByUserId($userId){
+        $user = $this->userRepository->where("id",$userId)->with("leadPipelines")->first();
+        $pipelines =$user?->leadPipelines;
         return $pipelines;
     }
-
     /**
      * get the default pipeline for all users
      */
-    public function getDefaultPipelineAllUsers()
-    {
+    public function getDefaultPipelineAllUsers(){
 
         return $this->all();
     }
+    /**
+     * get pipeline by user id
+     */
+    public function getPipelinesForUser($userId)
+    {
+        $pipelines = $this->userRepository->with('leadPipelines')->find($userId);
+        $pipelines = $pipelines->leadPipelines ? $pipelines->leadPipelines : [];
 
+        return $pipelines;
+    }
     /**
      * get all pipelines for user id
      */
-    public function getAllUserForPipelineId($pipelineId)
-    {
-        return $this->where('id', $pipelineId)->with('users')->first();
+    public function getAllUserForPipelineId($pipelineId){
+        return $this->where("id",$pipelineId)->with("users")->first();
     }
 }
